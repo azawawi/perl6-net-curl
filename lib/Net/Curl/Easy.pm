d44ccab909f4cd7ff3e627087fbd1ba4efc1ec48
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2015-11-03 12:09:13 +0200

    Convert to unit class for consistency

--- lib/Net/Curl/Easy.pm
+++ lib/Net/Curl/Easy.pm
@@ -2,34 +2,34 @@ use v6;
 
 use Net::Curl;
 
-class Net::Curl::Easy {
-	has $.url;
-	
-	method download {
-	
-		my $curl = curl_easy_init;
-		my $body = Buf.new; # or use Str
+unit class Net::Curl::Easy;
 
-		if $curl {
-			curl_easy_setopt($curl, CURLOPT_URL, $.url);
-			curl_easy_setopt($curl, CURLOPT_WRITEDATA, $body);
-			curl_easy_setopt($curl, CURLOPT_FOLLOWLOCATION, 0);
-	 
-				# Perform the request, res will get the return code
-			my $res = curl_easy_perform($curl);
+has $.url;
 
-			# Check for errors
-			if $res != CURLE_OK {
-				say "Error: curl_easy_perform() failed: " ~ 
-				curl_easy_strerror($res);
-			}
+method download {
 
-			# always cleanup 
-			curl_easy_cleanup($curl);
+	my $curl = curl_easy_init;
+	my $body = Buf.new; # or use Str
+
+	if $curl {
+		curl_easy_setopt($curl, CURLOPT_URL, $.url);
+		curl_easy_setopt($curl, CURLOPT_WRITEDATA, $body);
+		curl_easy_setopt($curl, CURLOPT_FOLLOWLOCATION, 0);
+
+			# Perform the request, res will get the return code
+		my $res = curl_easy_perform($curl);
+
+		# Check for errors
+		if $res != CURLE_OK {
+			say "Error: curl_easy_perform() failed: " ~
+			curl_easy_strerror($res);
 		}
 
-		return $body.decode('latin1');
+		# always cleanup
+		curl_easy_cleanup($curl);
 	}
+
+	return $body.decode('latin1');
 }
 
 # vim: ft=perl6
